fb05c5ac4aa554fc61bff69f2b26959d16be19be
Sebastian Gil <shiuxcode@gmail.com>
2021-08-30 09:32:03 -0500

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 // initiation
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.set('port', process.env.PORT || 8080)
 
 // middleware
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
