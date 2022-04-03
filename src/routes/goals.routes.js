37c060e88c7532a9ee5edca4b570b8775411c746
rokujs <shiuxcode@gmail.com>
2022-04-02 21:34:31 -0500

    Add middleware Authorization to User enpoints

--- src/routes/goals.routes.js
+++ src/routes/goals.routes.js
@@ -1,29 +1,29 @@
-const userExtractor = require('../middlewares/userExtractor')
 
 const { Router } = require('express')
 const router = Router()
 
+const userExtractor = require('../middlewares/userExtractor')
 const { createNewGoal, getAllGoals, getSingleGoal, wasDone, abandonGoal, deleteGoal, resumeGoal } = require('../controllers/goals.controller')
 
 // All goals
-router.get('/api/goals', getAllGoals)
+router.get('/api/goals', userExtractor, getAllGoals)
 
 // new Goal
 router.post('/api/goals/add', userExtractor, createNewGoal)
 
 // Single goal
-router.get('/api/goal/:id', getSingleGoal)
+router.get('/api/goal/:id', userExtractor, getSingleGoal)
 
 // abandon goal
-router.put('/api/goal/:id', abandonGoal)
+router.put('/api/goal/:id', userExtractor, abandonGoal)
 
 // done goal
-router.patch('/api/goal/:id', wasDone)
+router.patch('/api/goal/:id', userExtractor, wasDone)
 
 // delete goal
-router.delete('/api/goal/:id', deleteGoal)
+router.delete('/api/goal/:id', userExtractor, deleteGoal)
 
 // resume goal
-router.patch('/api/goal/resume/:id', resumeGoal)
+router.patch('/api/goal/resume/:id', userExtractor, resumeGoal)
 
 module.exports = router
